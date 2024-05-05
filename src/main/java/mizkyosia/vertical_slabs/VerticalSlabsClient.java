5f925423586bbab41422b32e713f580e519c1cf9
LeoLewandowski <leo.lewandowski@student.junia.com>
2024-05-05 20:34:39 +0200

    Mod Start

---
+++ src/main/java/mizkyosia/vertical_slabs/VerticalSlabsClient.java
@@ -0,0 +1,10 @@
+package mizkyosia.vertical_slabs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VerticalSlabsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
